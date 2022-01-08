0276854c4f0fd1169fc0b95dc0d48504aaff4caa
selenisri <91556917+selenisri@users.noreply.github.com>
2022-01-08 21:49:22 +0530

    updated from github ui
    updated from github ui techie

--- src/scripts/SoftAsserts.java
+++ src/scripts/SoftAsserts.java
@@ -13,7 +13,7 @@ public class SoftAsserts {
 	public void Testing()
 	{
 		
-		//working out on the softassert
+		//working out on the softassert //iam in github
 		
 		System.out.println("Hello I will work !");
 		softAssert.assertEquals(true, false); //Now even if this Assert Line Fails , Next will get Executed.
